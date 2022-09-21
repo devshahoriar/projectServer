2cab62513a90d1eedc8cf37693f9ecf08e753881
Shahoriar <shahoriar.dev@gmail.com>
2022-09-21 19:20:43 +0600

    first commit

---
+++ server.js
@@ -0,0 +1,21 @@
+const auth = require('json-server-auth')
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+var cors = require('cors')
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 9000;
+
+const rules = auth.rewriter({
+  users: 660,
+  teams: 660,
+})
+
+server.db = router.db
+server.use(cors());
+server.use(rules);
+server.use(auth);
+server.use(middlewares);
+server.use(router);
+
+server.listen(port);
